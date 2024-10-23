61fbc718f24ed0a275043e18bc972ffcd6110249
Thaherabegum <228A1A4222@risekrishnasaiprakasam.edu.in>
2024-10-23 10:55:22 +0530

    Update exm.java

--- 4222/exm.java
+++ 4222/exm.java
@@ -3,5 +3,6 @@ pubic class Hello
       public static void main(String[] args)
       {
           System.out.println("hello everyone");
+          System.out.println("Welcome to SOC Lab");
       }
 }
